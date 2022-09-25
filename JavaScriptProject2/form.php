211dbcee538e246e763110ee1028cf8268402768
jovanayc <jovanay.d.carter@gmail.com>
2022-09-25 19:00:08 -0400

    starting code of matching game project 2

---
+++ JavaScriptProject2/form.php
@@ -0,0 +1,16 @@
+<?php
+// define variables for php funcction
+$userid = "";
+
+//
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
---
+++ JavaScriptProject2/matching-game.js
@@ -0,0 +1,14 @@
+/*
+    - photos start all shoowing backside
+    - onclick, they will show faceside
+    - if 2 are showing face & they are not the same, then flip both back to backsides
+    - should have a set of photos in a file that are randomly selected and placed in different areas of the table
+    - photos should be place twice --> Maybe a array that iterates throuogh twice at random filling each table cell (4 x 4)
+    - stop game when all cards are matched or when countdown ends
+
+    Additional
+    - Countdown of 1 minute at top of the page
+    - Modal that comes on page if countdown gets to 0 before user finishes the game
+    - If user finishes the game before the timer --> get time
+    - Modal for winner that says they won & thier time; also shows top 3 positions in leaderboard & user names
+*/
