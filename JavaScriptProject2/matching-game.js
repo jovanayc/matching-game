2b28abb5dfd2c58848dd10588149f807645333b0
jovanayc <jovanay.d.carter@gmail.com>
2022-10-15 00:54:41 -0400

    trying to figure out what makes all the cards turn besides just one

--- JavaScriptProject2/matching-game.js
+++ JavaScriptProject2/matching-game.js
@@ -22,43 +22,41 @@ var index;
 const usedPhotos = []; // array of numbers already used
 var removeUsedPhoto;
 //var tableData = document.querySelectorAll(".table");//gets all table data elements
-//var imageIds = ["\"1\"","\"2\"","\"3\"","\"4\"","\"5\"","\"6\"","\"7\"","\"8\"","\"9\"","\"10\"","\"11\"","\"12\"","\"13\"","\"14\"","\"15\"","\"16\""];
-var imageIds = ["img1","img2","img3","img4","img5","img6","img7","img8","img9","img10","img11","img12","img13","img14","img15","img16"];
+//var imageIds = ["img1","img2","img3","img4","img5","img6","img7","img8","img9","img10","img11","img12","img13","img14","img15","img16"];
+var imageIds = document.getElementsByName('tImages');
 var tableCellIds = ["cell1","cell2","cell3","cell4","cell5","cell6","cell7","cell8","cell9", "cell10","cell11","cell12","cell13","cell14","cell15","cell16"];
 var chosenPhoto;
 
 
-function coverFunc(){//creates just to test getting once cover to change photos onclick
-   //logging that function is starting up
-   console.log('logging coverFunc');
+function flipImage(){ //this function change photos onclick; in the future it should only change 1 photo at a time
 
-   //adding random photo to images
-   //loop through table data by image attribute
-
-   //???if element td is clicked, then call click function & change only that cell
-         //then get chosen image
+   //GOAL: if element td is clicked, then call click function & change only that cell
+   var cellArr = document.getElementsByName("tableCell");
    var cell = document.getElementById(tableCellIds[0]);
-   cell.addEventListener('click', showRandomPhoto());
+   console.log(cellArr);
+   console.log(cell);
+   cell.addEventListener('click', showRandomPhoto()); //Shows random photo in selected cell when clicked
+   
 }
 
 function showRandomPhoto(){
-   for (j=0; j<16; j++){
+   //for (j=0; j<16; j++){//commented out to get images to stop loading on entire table on first click
       
       //choosing a random photo from the array using math random
       index = Math.floor(Math.random() * (imagesArray.length));// get random index # for Images array
       chosenPhoto = imagesArray[index]; //update chosen photo
       console.log(chosenPhoto);
 
-      console.log(imageIds[j]); // check that imageId is properly loading for the for loop
+      //console.log(imageIds[j]); // check that imageId is properly loading for the for loop
       
       //change element image attribute src to chosenPhoto
-      document.getElementById(imageIds[j]).src = chosenPhoto;//changes image to hidden image
+      imageIds[0].src = chosenPhoto;//changes image to hidden image
       console.log(`Starting Image Array length: ${imagesArray.length}`);//log starting image array length
       console.log(`Starting chosen photo: ${imagesArray[index]}`);
 
       removePhoto(index);
 
-   }
+  // }
 }
 
 function removePhoto(){
@@ -70,38 +68,25 @@ function removePhoto(){
    removeUsedPhoto = imagesArray.splice(index, 1);
    console.log(`Updated images arrray length after removal: ${imagesArray.length}`);
 }
-   /*
-   //get all table data elements
-   tableData = document.querySelectorAll("td");
-   console.log(tableData);
-   console.log(tableData.length);
-   console.log(tableData[0]);
 
-
-   tableData[0].setAttribute("img", chosenPhoto);
-
-   //set new name attribute
-   
-   for (i=0; i<tableData.length; i++){
-      tableData[i].setAttribute("img", chosenPhoto);
-      //console.log(tableData);
-      return tableData[i];
-   }
-   */
-
-
-function flipImage() { //should create a random index for src image array; adds nes element to the image file.
+/*
+function flipImage2() { //should create a random index for src image array; adds next element to the image file.
    var img = document.createElement('img')
    //index = Math.floor(Math.random() * 16);
    img.src = imagesArray[index];
    document.getElementById('image1B').appendChild(img).src;
 };
 
-/*
-   source= https://www.delftstack.com/howto/javascript/javascript-array-of-images/
-
-*/
-
 function timer(){
   alert('You lost, try again to match all the tiles!');
 }
+*/
+
+
+/* *********************************************
+TO-DOS
+1) How to get images to come up when that cell is clicked
+2) How to get images outside of top row to respond to on click events
+3) Go back & no hard-code image IDs
+4) Potential solve, put "Flip" button at the bottom of each td box that While clicked flips that cell's picture
+*/
